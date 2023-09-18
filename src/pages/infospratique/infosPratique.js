007d8e4543facfc4cb779dde83026ac182a7aa52
Swann_Jptz <63155968+RazAallgull60@users.noreply.github.com>
2023-09-18 17:51:27 +0200

    infospratiques_design_swann

--- src/pages/infospratique/infosPratique.js
+++ src/pages/infospratique/infosPratique.js
@@ -1,27 +1,34 @@
 import React from "react"
 import Geolocalization from "../../components/others/geolocalization/geolocalization"
+import "./infosPratique.scss"
 
 const InfosPratique = () => {
     return (
-        <div>
-            <h1>Accès</h1>
-            <p>
-                La salle le Billy se trouve sur la place de la mairie au centre de la ville de
-                Guingamp.
-            </p>
-            <Geolocalization />
-            <h3>Pour information, voici quelques durées moyennes de trajet :</h3>
-            <p>
-                Cherbourg : 1h00
-                <br />
-                Caen : 50mn
-                <br />
-                Rennes : 1h30mn
-                <br />
-                Le Mans : 2h40mn
-                <br />
-                Paris : 3h00
-            </p>
+        <div className="bloc-info">
+            <div className="container-info">
+                <h1 className="title-info">Accès</h1>
+                <p className="p">
+                    La salle le Billy se trouve sur la place de la mairie au centre de la ville de
+                    Guingamp.
+                </p>
+                <div className="map">
+                    <Geolocalization />
+                </div>
+                <h3 className="h3">
+                    Pour votre information, voici quelques durées moyennes de trajet :
+                </h3>
+                <p className="cities">
+                    <div>Cherbourg : 1h00</div>
+                    <br />
+                    <div>Caen : 50mn</div>
+                    <br />
+                    <div>Rennes : 1h30mn</div>
+                    <br />
+                    <div>Le Mans : 2h40mn</div>
+                    <br />
+                    <div>Paris : 3h00</div>
+                </p>
+            </div>
         </div>
     )
 }
