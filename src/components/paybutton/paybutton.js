48b46f19919fb422de28d8cd7976fbc9b9b114c3
Melly Lucas <melly.lucas32@yahoo.fr>
2023-09-21 12:49:17 +0200

    moncompteecheccplusstripeMelly

--- src/components/paybutton/paybutton.js
+++ src/components/paybutton/paybutton.js
@@ -1,10 +1,13 @@
-import React, { useState } from "react"
+import React, { useState, useContext } from "react"
 import Button from "../others/button/button"
 import CheckoutService from "../../services/checkout.service"
 import "../../ticket.scss"
+import userService from "../../services/user.service"
+import AuthContext from "../../context/AuthContext"
 
 const Paybutton = ({ event }) => {
     const [showExternalContent, setShowExternalContent] = useState(true)
+    const { userContext } = useContext(AuthContext)
 
     // const xx = () => {
     //     const timer = setTimeout(() => {
@@ -19,20 +22,51 @@ const Paybutton = ({ event }) => {
     // }
 
     const handleCheckout = () => {
+        // var tab = []
+        var user = {}
+        console.log(userContext.id)
+        userService
+            .getUser(userContext.id)
+            .then(data => {
+                console.log(data)
+                CheckoutService.createCheckoutSession(token, tab, event.id, userContext.id)
+                    .then(res => {
+                        if (res.url) {
+                            window.location.href = res.url
+                        }
+                    })
+                    .catch(err => console.log(err))
+                tab = data.events_user
+            })
+            .catch(err => console.log(err))
         const token = "c70f191c-d31b-4530-8d04-f8c81ce10b56"
         var tab = []
         tab.push(event)
-        CheckoutService.createCheckoutSession(token, tab, 1)
-            .then(res => {
-                console.log(res)
-                if (res.url) {
-                    window.location.replace(res.url)
-                }
-                // if (res.url) {
-                //     window.location.href = res.url
-                // }
-            })
-            .catch(err => console.log(err))
+        // CheckoutService.createCheckoutSession(token, tab, 1)
+        //     .then(res => {
+        //         // console.log(res)
+        //         // if (res.url) {
+        //         //     var tab = []
+        //         //     userService
+        //         //         .getUser(event.id)
+        //         //         .then(data => {
+        //         //             tab = data.events_user
+        //         //         })
+        //         //         .catch(err => console.log(err))
+        //         //     tab.push(event)
+        //         //     userService
+        //         //         .updateUser(event.id, tab)
+        //         //         .then(data => {
+        //         //             console.log(data)
+        //         //         })
+        //         //         .catch(err => console.log(err))
+        //         //     window.location.replace(res.url)
+        //         // }
+        //         if (res.url) {
+        //             window.location.href = res.url
+        //         }
+        //     })
+        //     .catch(err => console.log(err))
     }
     return (
         <div>
