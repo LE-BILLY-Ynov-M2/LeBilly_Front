7491135b9dd92a9c798a24b9641572ce4189a1f6
Melly Lucas <melly.lucas32@yahoo.fr>
2023-09-21 13:04:28 +0200

    correctionstripe

--- src/components/paybutton/paybutton.js
+++ src/components/paybutton/paybutton.js
@@ -22,14 +22,16 @@ const Paybutton = ({ event }) => {
     // }
 
     const handleCheckout = () => {
-        // var tab = []
+        var tab = []
         var user = {}
+        tab.push(event)
         console.log(userContext.id)
+        var token = userContext.token
         userService
             .getUser(userContext.id)
             .then(data => {
                 console.log(data)
-                CheckoutService.createCheckoutSession(token, tab, event.id, userContext.id)
+                CheckoutService.createCheckoutSession(token, tab, 1, userContext.id)
                     .then(res => {
                         if (res.url) {
                             window.location.href = res.url
@@ -39,9 +41,26 @@ const Paybutton = ({ event }) => {
                 tab = data.events_user
             })
             .catch(err => console.log(err))
-        const token = "c70f191c-d31b-4530-8d04-f8c81ce10b56"
-        var tab = []
-        tab.push(event)
+        // tab.push(event)
+        // console.log("tab debut")
+        // console.log(tab)
+        // console.log("tab fin")
+        // var new_user = {
+        //     events_user: tab,
+        // }
+        // console.log("debut newuser")
+        // console.log(new_user)
+        // console.log("fin userevnet")
+        // userService
+        //     .updateUser(userContext.id, new_user)
+        //     .then(data => {
+        //         console.log(data)
+        //     })
+        //     .catch(err => console.log(err))
+        // console.log(user)
+        // const token = "c70f191c-d31b-4530-8d04-f8c81ce10b56"
+        // var tab = []
+        // tab.push(event)
         // CheckoutService.createCheckoutSession(token, tab, 1)
         //     .then(res => {
         //         // console.log(res)
